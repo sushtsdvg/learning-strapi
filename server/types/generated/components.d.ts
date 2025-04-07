ae53afa946ab110abbbc6b6fabf81e45181ed277
sushtsdvg <sushtsdvg@gmail.com>
2025-04-07 09:10:28 +0530

    added styles

--- server/types/generated/components.d.ts
+++ server/types/generated/components.d.ts
@@ -1,3 +1,20 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ElementsLogo extends Struct.ComponentSchema {
+  collectionName: 'components_elements_logos';
+  info: {
+    displayName: 'Logo';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images'>;
+    logoText: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'elements.logo': ElementsLogo;
+    }
+  }
+}
